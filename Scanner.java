92a21abb4ddf80df79518d297856cc5473314180
Poonam Nainwal <77105745+PoonamNainwal@users.noreply.github.com>
2021-04-27 20:42:09 +0530

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Scanner {
+
+    public static void main(String[] args) {
+        String name;
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("May I know your Name: ");
+        name=sc.nextLine();
+        
+        System.out.println("Welcome "+name);
+    }
+    
+}
